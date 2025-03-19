d57e726705deade94e3d9fede698ca8a63fad0ab
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-03-19 22:05:05 +0100

    feat: add graphql client

---
+++ src/graphql/client.ts
@@ -0,0 +1,11 @@
+import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+
+export const client = new ApolloClient({
+  link: new HttpLink({
+    uri: import.meta.env.VITE_GITHUB_API_URL,
+    headers: {
+      Authorization: `Bearer ${import.meta.env.VITE_GITHUB_TOKEN}`,
+    },
+  }),
+  cache: new InMemoryCache(),
+});

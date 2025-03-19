f901710e703fce24d2c7ccc6fe7153ae74404740
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-03-19 22:05:22 +0100

    feat: add geat react issue query

---
+++ src/core/Issue/infrastructure/GraphqlGetIssues.ts
@@ -0,0 +1,19 @@
+import { gql } from "@apollo/client";
+
+export const GET_REACT_ISSUES = gql`
+  query GetIssues($query: String!) {
+    search(query: $query, type: ISSUE, first: 10) {
+      edges {
+        node {
+          ... on Issue {
+            id
+            title
+            body
+            state
+            url
+          }
+        }
+      }
+    }
+  }
+`;

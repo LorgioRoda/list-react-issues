8d60315420213f387308d82ab3bcc0bd90354c3d
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-03-20 22:56:44 +0100

    feat: add new getComments

---
+++ src/core/comments/infrastructure/interceptors/getComments.ts
@@ -0,0 +1,20 @@
+import { client } from "../../../../graphql/client";
+import { Comment } from "../../domain/Comment";
+import { commentsAdapterFromGraphQL } from "../adapter/commentsAdapterFromGraphQL";
+import { GET_ISSUE_COMMENTS } from "../GraphqlGetComments";
+
+export const getComments = async (issueId: string): Promise<Comment[]> => {
+    try {
+      const { data } = await client.query({
+        query: GET_ISSUE_COMMENTS,
+        variables: { issueId },
+      });
+  
+     const comments = commentsAdapterFromGraphQL(data)
+     console.log(comments);
+     return comments
+    } catch (error) {
+      console.error("GraphQL Error:", error);
+      return [];
+    }
+  };

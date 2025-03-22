3aa86536bbafb6a697d8c8da638f5d071f2708c7
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-03-22 21:35:18 +0100

    test: add mock comments

---
+++ test/mocks/comments.ts
@@ -0,0 +1,19 @@
+export const mockComment = {
+    node: {
+      comments: {
+        edges: [
+          {
+            node: {
+              id: "c1",
+              body: "Nice issue!",
+              createdAt: "2024-01-01T10:00:00Z",
+              author: {
+                login: "user123",
+                avatarUrl: "https://avatars.com/u123",
+              },
+            },
+          },
+        ],
+      },
+    },
+  };

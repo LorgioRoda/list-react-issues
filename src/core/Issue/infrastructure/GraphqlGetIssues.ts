cd8b35b52aee9528be8a5b1836a637a0c2ffc0cc
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-03-22 01:34:47 +0100

    feat: update query 20 first

--- src/core/Issue/infrastructure/GraphqlGetIssues.ts
+++ src/core/Issue/infrastructure/GraphqlGetIssues.ts
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 export const GET_REACT_ISSUES = gql`
   query GetIssues($query: String!) {
-    search(query: $query, type: ISSUE, first: 10) {
+    search(query: $query, type: ISSUE, first: 20) {
       edges {
         node {
           ... on Issue {

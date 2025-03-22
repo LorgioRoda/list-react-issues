cc0c7ba2adf207fc6edae85f2188b9d2ffaae175
Lorgio Roda <lorgiorodaroca@gmail.com>
2025-03-23 00:31:59 +0100

    feat: add theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 import React from 'react'
-import { ApolloProvider, gql} from '@apollo/client';
+import './index.css'
+import { ApolloProvider} from '@apollo/client';
 import { client } from './graphql/client';
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import { theme } from './theme/theme';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
   <ApolloProvider client={client}>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </ApolloProvider>
   </StrictMode>,
 )
